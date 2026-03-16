c042be670bae68f983bb093eb23eed2d3f5c5107
drzzln <drzzln@protonmail.com>
2026-03-15 20:55:08 -0400

    fix flake.nix structure and add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "Akeyless .NET CloudId provider for cloud authentication to Akeyless";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
